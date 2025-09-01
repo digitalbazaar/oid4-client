581044c179d0e88269432f74fa87ce9207b8d57f
Dave Longley <dlongley@digitalbazaar.com>
2025-08-31 21:01:26 -0400

    Start adding ISO 18013-7 unit test.

---
+++ tests/mdlUtils.js
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2025 Digital Bazaar, Inc. All rights reserved.
+ */
+import * as base64url from 'base64url-universal';
+import {
+  DataItem, DeviceResponse, Document, MDoc, /*parse,*/ Verifier
+} from '@auth0/mdl';
+import {base64Encode} from '../lib/util.js';
+
+const VC_CONTEXT_2 = 'https://www.w3.org/ns/credentials/v2';
+
+const MDL_NAMESPACE = 'org.iso.18013.5.1';
+const MDOC_TYPE_MDL = `${MDL_NAMESPACE}.mDL`;
+
+export async function createDeviceResponse({
+  presentationDefinition,
+  mdoc, sessionTranscript, devicePrivateJwk
+} = {}) {
+  devicePrivateJwk = {alg: 'ES256', ...devicePrivateJwk};
+  // `sessionTranscript` has:
+  // {mdocGeneratedNonce, clientId, responseUri, verifierGeneratedNonce}
+  const deviceResponse = await DeviceResponse.from(mdoc)
+    .usingPresentationDefinition(presentationDefinition)
+    .usingSessionTranscriptForOID4VP(
+      sessionTranscript.mdocGeneratedNonce,
+      sessionTranscript.clientId,
+      sessionTranscript.responseUri,
+      sessionTranscript.verifierGeneratedNonce)
+    .authenticateWithSignature(devicePrivateJwk, 'ES256')
+    .sign();
+  //console.log('Device response', deviceResponse);
+
+  return deviceResponse;
+}
+
+export async function createPresentation({
+  presentationDefinition,
+  mdoc, sessionTranscript, devicePrivateJwk
+} = {}) {
+  // `sessionTranscript` has:
+  // {mdocGeneratedNonce, clientId, responseUri, verifierGeneratedNonce}
+  const deviceResponse = await createDeviceResponse({
+    mdoc, presentationDefinition, sessionTranscript, devicePrivateJwk
+  });
+
+  // FIXME: define a base64url-encoded mdl vp token mime type?
+  const encodedDeviceResponse = deviceResponse.encode();
+  const vpToken = base64url.encode(encodedDeviceResponse);
+  // console.log('device side: device response cbor', encodedDeviceResponse);
+  // console.log(vpToken, 'vpToken');
+
+  return {
+    '@context': [VC_CONTEXT_2],
+    id: `data:application/mdl-vp-token,${vpToken}`,
+    type: 'EnvelopedVerifiablePresentation'
+  };
+}
+
+export async function generateDeviceKeyPair() {
+  // FIXME: generate new key pair each time
+  const publicJwk = {
+    kty: 'EC',
+    x: 'QiUaYhZak1NubJEphQWmafykivrD80D2IpwqkkCU0oQ',
+    y: 'sdNfR3813hzaUqF3-kWWOjI1xtSEqb93-graWFK-bA4',
+    crv: 'P-256'
+  };
+  const privateJwk = {
+    ...publicJwk,
+    d: 'V729tbSdAGAL34Gqt2lGFM0Y9qrxILDUVheFduEkgFU'
+  };
+  return {publicJwk, privateJwk};
+}
+
+export async function issue({
+  issuerPrivateJwk, issuerCertificate,
+  devicePublicJwk
+} = {}) {
+  issuerPrivateJwk = {alg: 'ES256', ...issuerPrivateJwk};
+  const document = await new Document(MDOC_TYPE_MDL)
+    .addIssuerNameSpace(MDL_NAMESPACE, {
+      family_name: 'FamilyName',
+      given_name: 'GivenName',
+      birth_date: '1990-01-01',
+      age_over_21: true
+    })
+    .useDigestAlgorithm('SHA-256')
+    .addValidityInfo({signed: new Date()})
+    .addDeviceKeyInfo({deviceKey: devicePublicJwk})
+    .sign({
+      issuerPrivateKey: issuerPrivateJwk,
+      issuerCertificate,
+      kid: issuerPrivateJwk.kid,
+      alg: 'ES256'
+    });
+  return new MDoc([document]);
+}
+
+export async function verifyPresentation({
+  deviceResponse, sessionTranscript, trustedCertificates
+} = {}) {
+  // uncomment to debug:
+  /*const parsed = parse(deviceResponse);
+  const issuerCertificate = parsed.documents?.[0]
+    .issuerSigned?.issuerAuth?.certificate;
+  console.log('issuer certificate', issuerCertificate);*/
+
+  // produced on the verifier side
+  const encodedSessionTranscript = _encodeSessionTranscript(sessionTranscript);
+
+  const verifier = new Verifier(trustedCertificates);
+  // console.log('Getting diagnostic information...');
+  // const diagnostic = await verifier.getDiagnosticInformation(
+  //   deviceResponse, {encodedSessionTranscript});
+  // console.debug('Diagnostic information:', diagnostic);
+
+  try {
+    const mdoc = await verifier.verify(deviceResponse, {
+      encodedSessionTranscript
+    });
+    // console.log('Verification succeeded!');
+    // console.log('Verified mdoc', mdoc);
+    // console.log('DeviceSignedDocument', mdoc.documents[0]);
+
+    // express cbor-encoded mdoc as an enveloped VC in a VP
+    const encodedMdoc = mdoc.encode();
+    const b64Mdl = base64Encode(encodedMdoc);
+    return {
+      '@context': [VC_CONTEXT_2],
+      type: 'VerifiablePresentation',
+      verifiableCredential: [{
+        id: `data:application/mdl;base64,${b64Mdl}`,
+        type: 'EnvelopedVerifiableCredential'
+      }]
+    };
+  } catch(err) {
+    //console.error('Verification failed:', err);
+    return;
+  }
+}
+
+function _encodeSessionTranscript(sessionTranscript) {
+  const {
+    mdocGeneratedNonce,
+    clientId,
+    responseUri,
+    verifierGeneratedNonce
+  } = sessionTranscript;
+  const encoded = DataItem.fromData([
+    // deviceEngagementBytes
+    null,
+    // eReaderKeyBytes
+    null,
+    [mdocGeneratedNonce, clientId, responseUri, verifierGeneratedNonce],
+  ]);
+  return DataItem.fromData(encoded).buffer;
+}
