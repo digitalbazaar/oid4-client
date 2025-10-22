620f849c6906d3e4ed30afc7c15c59245d51947d
Dave Longley <dlongley@digitalbazaar.com>
2025-10-22 18:27:20 -0400

    Add QueryByExample => DCQL generation code.

--- tests/unit/query.dcql.spec.js
+++ tests/unit/query.dcql.spec.js
@@ -11,9 +11,126 @@ const {expect} = chai;
 
 describe('query.dcql', () => {
   describe('QueryByExample => DCQL', () => {
-    it('should pass', async () => {
-      const result = _fromQueryByExampleQuery({});
-      expect(result).to.exist;
+    it('should process deep query', async () => {
+      const dcqlCredentialQuery = _fromQueryByExampleQuery({
+        credentialQuery: {
+          reason: `Please present your child's birth certificate to complete ` +
+            'the verification process.',
+          example: {
+            '@context': [
+              'https://www.w3.org/ns/credentials/v2',
+              'https://w3id.org/vital-records/v1rc4'
+            ],
+            type: [
+              'BirthCertificateCredential'
+            ],
+            credentialSubject: {
+              type: 'BirthCertificate',
+              certifier: {},
+              newborn: {
+                name: '',
+                birthDate: '',
+                parent: [{
+                  name: 'John Doe'
+                }]
+              }
+            }
+          }
+        }
+      });
+      expect(dcqlCredentialQuery.id).to.exist;
+      expect(dcqlCredentialQuery.format).to.eql('ldp_vc');
+      expect(dcqlCredentialQuery.meta.type_values).to.deep.equal([
+        'https://www.w3.org/2018/credentials#VerifiableCredential'
+      ]);
+      expect(dcqlCredentialQuery.claims).to.deep.equal([
+        {
+          path: ['@context', 0],
+          values: ['https://www.w3.org/ns/credentials/v2']
+        },
+        {
+          path: ['@context', 1],
+          values: ['https://w3id.org/vital-records/v1rc4']
+        },
+        {
+          path: ['type'],
+          values: ['BirthCertificateCredential']},
+        {
+          path: ['credentialSubject', 'type'],
+          values: ['BirthCertificate']
+        },
+        {
+          path: ['credentialSubject', 'certifier' ]},
+        {
+          path: ['credentialSubject', 'newborn', 'name']},
+        {
+          path: ['credentialSubject', 'newborn', 'birthDate']},
+        {
+          path: ['credentialSubject', 'newborn', 'parent', 0, 'name'],
+          values: ['John Doe']
+        }
+      ]);
+    });
+    it('should process deep query and nullyify indices', async () => {
+      const dcqlCredentialQuery = _fromQueryByExampleQuery({
+        nullyifyArrayIndices: true,
+        credentialQuery: {
+          reason: `Please present your child's birth certificate to complete ` +
+            'the verification process.',
+          example: {
+            '@context': [
+              'https://www.w3.org/ns/credentials/v2',
+              'https://w3id.org/vital-records/v1rc4'
+            ],
+            type: [
+              'BirthCertificateCredential'
+            ],
+            credentialSubject: {
+              type: 'BirthCertificate',
+              certifier: {},
+              newborn: {
+                name: '',
+                birthDate: '',
+                parent: [{
+                  name: 'John Doe'
+                }]
+              }
+            }
+          }
+        }
+      });
+      expect(dcqlCredentialQuery.id).to.exist;
+      expect(dcqlCredentialQuery.format).to.eql('ldp_vc');
+      expect(dcqlCredentialQuery.meta.type_values).to.deep.equal([
+        'https://www.w3.org/2018/credentials#VerifiableCredential'
+      ]);
+      expect(dcqlCredentialQuery.claims).to.deep.equal([
+        {
+          path: ['@context', 0],
+          values: ['https://www.w3.org/ns/credentials/v2']
+        },
+        {
+          path: ['@context', 1],
+          values: ['https://w3id.org/vital-records/v1rc4']
+        },
+        {
+          path: ['type'],
+          values: ['BirthCertificateCredential']},
+        {
+          path: ['credentialSubject', 'type'],
+          values: ['BirthCertificate']
+        },
+        {
+          path: ['credentialSubject', 'certifier' ]},
+        {
+          path: ['credentialSubject', 'newborn', 'name']},
+        {
+          path: ['credentialSubject', 'newborn', 'birthDate']},
+        {
+          path: ['credentialSubject', 'newborn', 'parent', null, 'name'],
+          values: ['John Doe']
+        }
+      ]);
     });
   });
 });
